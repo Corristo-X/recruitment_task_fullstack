5fdab04790b7a5e6227d0ff59ed8b6d315d4e9c7
Daniel Talarek <daniel@soffee.pl>
2024-07-21 11:28:12 +0200

    Add front-end navigation and route for exchange rates with optional date parameter

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -3,30 +3,34 @@
 import React, {Component} from 'react';
 import {Route, Redirect, Switch, Link} from 'react-router-dom';
 import SetupCheck from "./SetupCheck";
+import ExchangeRates from "./exchange/ExchangeRates";
 
 class Home extends Component {
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className={"navbar-brand"} to={"#"}> Telemedi Zadanko </Link>
-                    <div id="navbarText">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className={"nav-link"} to={"/setup-check"}> React Setup Check </Link>
-                            </li>
-
-                        </ul>
-                    </div>
-                </nav>
-                <Switch>
-                    <Redirect exact from="/" to="/setup-check" />
-                    <Route path="/setup-check" component={SetupCheck} />
-                </Switch>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className={"navbar-brand"} to={"#"}> Telemedi Zadanko </Link>
+          <div id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className={"nav-link"} to={"/setup-check"}> React Setup Check </Link>
+              </li>
+              <li>
+                <Link className={"nav-link"} to="/exchange-rates">Exchange Rates</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Redirect exact from="/" to="/setup-check"/>
+          <Route path="/setup-check" component={SetupCheck}/>
+          <Route path="/exchange-rates/:date?" component={ExchangeRates} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default Home;
